aedb05a003cd3b03a9ed5fa4d4730fdff38cf3c9
Naharie <oneofthosewhoescaped@gmail.com>
2022-02-02 19:53:45 -0700

    More work on functional

---
+++ FunctionalPreview/Functional.Preview.fs
@@ -0,0 +1,16 @@
+﻿module Functional.Preview
+
+open System
+open System.Threading.Tasks
+open Microsoft.DotNet.Interactive
+open Microsoft.DotNet.Interactive.Formatting
+
+let register () =
+    Formatter.Register<DateTime>((fun (date: DateTime) (writer: IO.TextWriter) ->
+        writer.Write($"""
+            <div style="display: inline-block; padding: 5px; border-radius: 5px; background-color: lightgray;">
+                <span style="color: #303030;">Day: </span>
+                <span style="color: #1171c2;">{date.ToString "ddd, MMM d, yyyy"}</span>
+            </div>
+        """)
+    ), "text/html")
