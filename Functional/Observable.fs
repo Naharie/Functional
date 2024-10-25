689d0e813c081baaaf331f0cadd69cd3458df596
Naharie <oneofthosewhoescaped@gmail.com>
2024-10-24 21:17:59 -0600

    Add nullity checks to Observable helpers.

--- Functional/Observable.fs
+++ Functional/Observable.fs
@@ -10,6 +10,8 @@ open Purity
 /// <param name="observer">A function that taking a cancellation function provides a <see cref="System.IObserver" /> </param>
 /// <param name="observable">The observable that the resulting observer is subscribed to.</param>
 let watch (observer: (unit -> unit) -> IObserver<'t>) (observable: IObservable<'t>) =
+    ensureNotNull (nameof observable) observable
+    
     withRecursive (fun (subscription: IDisposable Lazy) ->
         let cancel () = subscription.Value.Dispose()
         observable.Subscribe (observer cancel)
@@ -21,6 +23,8 @@ let watch (observer: (unit -> unit) -> IObserver<'t>) (observable: IObservable<'
 /// <param name="handler">A function that will be called once with the next value from the observable.</param>
 /// <param name="observable">The observable that the value will come from.</param>
 let once (handler: 't -> unit) observable =
+    ensureNotNull (nameof observable) observable
+    
     let mutable alive = true
     let cancel (disposable: IDisposable) =
         alive <- false
